b937ec70ad87d17cf3bd67def74c948a5b6ff29b
Jorge Paul <jserranopaul@gmail.com>
2021-03-29 13:28:11 +0200

    Get, get single and post

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,16 +1,51 @@
 import pool from '../db/pg.js';
 
 export const getAllRecipes = async (req, res) => {
-  const { rows } = await pool.query('SELECT NOW();');
-  res.send(rows);
+  try {
+    const { rowCount, rows } = await pool.query('SELECT * FROM recipes');
+    if (!rowCount) return res.json({ message: 'No recipes' });
+    const recipes = rows.map(row => ({ ...row, ingredients: row.ingredients.split(',') }));
+    res.json(recipes);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
 export const getSingleRecipe = async (req, res) => {
-  res.send('Recipe');
+  try {
+    const { id } = req.params;
+    const { rowCount, rows } = await pool.query('SELECT * FROM recipes WHERE id=$1', [id]);
+    if (!rowCount)
+      return res.status(404).json({ message: `Recipe with id of ${id} doesn't exists` });
+    const recipe = { ...rows[0], ingredients: rows[0].ingredients.split(',') };
+    res.json(recipe);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
 export const createRecipe = async (req, res) => {
-  res.send('Create');
+  try {
+    const { name, description, imgurl, instructions, ingredients } = req.body;
+    if (!name || !description || !imgurl || !instructions || !ingredients)
+      return res.status(400).json({
+        message: 'Name, description, image, instructions and ingredients are mandatory fields'
+      });
+    const { rows } = await pool.query(
+      `INSERT INTO recipes(
+        name,
+        description,
+        imgUrl,
+        instructions,
+        ingredients) 
+        VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+      [name, description, imgurl, instructions, ingredients]
+    );
+    const recipe = { ...rows[0], ingredients: rows[0].ingredients.split(',') };
+    res.json(recipe);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
 export const updateRecipe = async (req, res) => {
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
-
+app.use(express.json());
 app.use('/recipes', recipes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`.yellow));
